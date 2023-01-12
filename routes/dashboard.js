303b8d551a594ba542583595483643954764391e
NotMeAgain10 <dev@isupport.com.gr>
2023-01-12 11:51:34 +0200

    single production

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -31,6 +31,7 @@ router.post('/deletecatPost',dashboardController.deletecatPost);
 router.post('/getProduction',dashboardController.getProduction);
 router.post('/addProduction',dashboardController.addProduction);
 router.post('/sendProduction',dashboardController.sendProduction);
+router.post('/getSingleProduction',dashboardController.getSingleProduction);
 // router.post('/updateProduction',dashboardController.updateProduction);
 // router.post('/deleteProduction',dashboardController.deleteProduction);
 // production Line 
